9e22e4342c1351de68b14aa3037027cba4dfc7af
Patrik Spathon <patrik@spathon.com>
2024-06-02 18:48:58 +0200

    Add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import './reset.css'
 import './globals.css'
 import Image from 'next/image'
@@ -16,11 +17,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <div className="app">
           <div>{children}</div>
           <div className="footer">
-            <Image src="/api_logo_pwrdBy_strava_horiz_light.svg" className="m-auto" width={107} height={20} alt="Powered by Strava" />
+            <Image
+              src="/api_logo_pwrdBy_strava_horiz_light.svg"
+              className="m-auto"
+              width={107}
+              height={20}
+              alt="Powered by Strava"
+            />
             &copy; Spathon | <a href="https://github.com/spathon/StravaStats">Code</a>
           </div>
         </div>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
