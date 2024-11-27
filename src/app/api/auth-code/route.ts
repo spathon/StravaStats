ddf536a44fbd5a6c5e21de3c6490456bbfb2f4c8
spathon <patrik@spathon.com>
2024-11-27 09:49:34 +0100

    Update packages an change to biome

--- src/app/api/auth-code/route.ts
+++ src/app/api/auth-code/route.ts
@@ -1,15 +1,15 @@
-import { kv } from '@vercel/kv'
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
-import { type NextRequest } from 'next/server'
+import { kv } from '@vercel/kv';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import type { NextRequest } from 'next/server';
 
 function sendResponse(str: string) {
-  return new Response(str, { status: 500 })
+  return new Response(str, { status: 500 });
 }
 
 export async function GET(req: NextRequest) {
-  const code = req.nextUrl.searchParams.get('code')
-  if (!code) return sendResponse("Ah ah ah, you didn't say the magic word")
+  const code = req.nextUrl.searchParams.get('code');
+  if (!code) return sendResponse("Ah ah ah, you didn't say the magic word");
 
   const userBlob = await fetch('https://www.strava.com/api/v3/oauth/token', {
     method: 'POST',
@@ -20,20 +20,24 @@ export async function GET(req: NextRequest) {
       code,
       grant_type: 'authorization_code',
     }),
-  })
-  if (!userBlob.ok || userBlob.status !== 200) return sendResponse('Could not get user')
-  const userResp = (await userBlob.json()) as StravaTokenResp
+  });
+  if (!userBlob.ok || userBlob.status !== 200)
+    return sendResponse('Could not get user');
+  const userResp = (await userBlob.json()) as StravaTokenResp;
 
-  const resp = await fetch('https://www.strava.com/api/v3/athlete/activities?per_page=100', {
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${userResp.access_token}`,
+  const resp = await fetch(
+    'https://www.strava.com/api/v3/athlete/activities?per_page=100',
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userResp.access_token}`,
+      },
     },
-  })
+  );
   if (!userBlob.ok || userBlob.status !== 200) {
-    return sendResponse('You need to approve activities to use this app')
+    return sendResponse('You need to approve activities to use this app');
   }
-  const activities = (await resp.json()) as StravaActivity[]
+  const activities = (await resp.json()) as StravaActivity[];
 
   const user = {
     id: userResp.athlete.id,
@@ -42,19 +46,20 @@ export async function GET(req: NextRequest) {
     country: userResp.athlete.country,
     profile_medium: userResp.athlete.profile_medium,
     activities,
-  }
+  };
 
-  const userUuid = crypto.randomUUID()
-  const fiveDays = 5 * 24 * 60 * 60
-  await kv.set(`user:${userUuid}`, JSON.stringify(user), { ex: fiveDays })
+  const userUuid = crypto.randomUUID();
+  const fiveDays = 5 * 24 * 60 * 60;
+  await kv.set(`user:${userUuid}`, JSON.stringify(user), { ex: fiveDays });
 
-  cookies().set({
+  const cookieStore = await cookies();
+  cookieStore.set({
     name: 'user',
     value: `${userUuid}`,
     httpOnly: true,
     secure: true,
     sameSite: 'lax',
-  })
+  });
 
-  redirect('/')
+  redirect('/');
 }
--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,24 +1,24 @@
-import { kv } from '@vercel/kv'
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
+import { kv } from '@vercel/kv';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
-export const runtime = 'edge'
+export const runtime = 'edge';
 
 export async function GET() {
-  const cookieStore = cookies()
-  const userId = cookieStore.get('user')
-  if (!userId?.value) return redirect('/')
+  const cookieStore = await cookies();
+  const userId = cookieStore.get('user');
+  if (!userId?.value) return redirect('/');
 
-  await kv.del(`user:${userId?.value}`)
+  await kv.del(`user:${userId?.value}`);
 
-  cookies().set({
+  cookieStore.set({
     name: 'user',
     value: '',
     expires: new Date('1999-12-31'),
     httpOnly: true,
     secure: true,
     sameSite: 'lax',
-  })
+  });
 
-  redirect('/')
+  redirect('/');
 }
